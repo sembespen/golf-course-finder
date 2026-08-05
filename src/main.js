e2bfeba4ad99c41fb23e69e8f9dabbd6fd70b153
Espen Semb <semb.espen@gmail.com>
2026-08-05 15:53:02 +0200

    Set up basic search response

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 import './style.css'
 
 console.log("Golf Course Finder started");
+
+const form = document.querySelector("#course-search-form");
+const searchStatus = document.querySelector("#search-status");
+let searchStatusMessage = document.createElement("p");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const input = form.querySelector("#course-search");
+
+    searchStatusMessage.innerHTML = "Searching...";
+    searchStatus.append(searchStatusMessage)
+
+    setTimeout(() => {
+        searchStatusMessage.innerHTML = "You searched for '" + input.value + "'";
+    }, 1000);
+});
