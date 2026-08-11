394fbbb2a696cf2629a88d366e89cccf25995270
Espen Semb <semb.espen@gmail.com>
2026-08-11 12:21:30 +0200

    Update search results when controls change

--- src/main.js
+++ src/main.js
@@ -13,6 +13,43 @@ const searchStatus = document.querySelector("#search-status");
 const searchStatusMessage = document.createElement("p");
 const searchResults = document.querySelector("#course-results");
 
+const countryPreference = document.querySelector('select[name="country-preference"]');
+const holePreference = document.querySelectorAll('input[name="hole-preference"]');
+const sortPreference = document.querySelector('select[name=sort-preference]');
+
+let currentSearchResults = [];
+let currentSearchQuery = "";
+
+function updateResults() {
+    searchResults.innerHTML = "";
+
+    let courses = [...currentSearchResults]; // technically can do courses=currentSearchResults; no mutation will happen
+
+    const currentHolePreference = document.querySelector('input[name="hole-preference"]:checked').value;
+
+    courses = filterCourses(courses, countryPreference.value, currentHolePreference);
+
+    courses = sortCourses(courses, sortPreference.value);
+
+    if (courses.length === 0) {
+        searchStatusMessage.textContent = `No courses found for "${currentSearchQuery}".`;
+
+        return; 
+    }
+    
+    searchStatusMessage.textContent = `${courses.length} course${courses.length === 1 ? "" : "s"} found for "${currentSearchQuery}".`
+
+    renderCourses(searchResults, courses);
+}
+
+countryPreference.addEventListener("change", updateResults);
+
+holePreference.forEach((element) => {
+    element.addEventListener("change", updateResults);
+});
+
+sortPreference.addEventListener("change", updateResults);
+
 searchForm.addEventListener("submit", async (event) => {
     event.preventDefault();
 
@@ -21,27 +58,12 @@ searchForm.addEventListener("submit", async (event) => {
     searchStatusMessage.textContent = "Searching...";
     searchStatus.append(searchStatusMessage)
 
-    const searchInputValue = searchInput.value;
+    currentSearchQuery = searchInput.value;
 
     try {
-        let courses = await searchCourses(searchInputValue);
+        currentSearchResults = await searchCourses(currentSearchQuery);
 
-        const countryPreference = document.querySelector('select[name="country-preference"]').value;
-        const holePreference = document.querySelector('input[name="hole-preference"]:checked').value;
-        courses = filterCourses(courses, countryPreference, holePreference);
-
-        const sortPreference = document.querySelector('select[name=sort-preference]').value;
-        courses = sortCourses(courses, sortPreference);
-
-        if (courses.length === 0) {
-            searchStatusMessage.textContent = `No courses found for "${searchInputValue}".`;
-
-            return; 
-        }
-        
-        searchStatusMessage.textContent = `${courses.length} course${courses.length === 1 ? "" : "s"} found for "${searchInputValue}".`
-
-        renderCourses(searchResults, courses);
+        updateResults();
     } catch(error) {
         searchStatusMessage.textContent = "Something went wrong. Please try again.";
 
